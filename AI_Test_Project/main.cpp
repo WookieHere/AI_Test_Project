2616a2cdefb4f7e7dfdadbf4d15fe11187983d61
Luke Nasby <wookiehere99@gmail.com>
2020-09-22 15:33:24 -0400

    Initial Commit

---
+++ AI_Test_Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AI_Test_Project
+//
+//  Created by Luke Nasby on 9/22/20.
+//  Copyright © 2020 Luke Nasby. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
